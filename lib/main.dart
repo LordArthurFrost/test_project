417c306e00ff5f943ffd4df80c2f498c2ba2e626
Arthur <arthur.lord.frost@gmail.com>
2021-06-06 19:09:51 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container(
-      padding: EdgeInsets.all(30),
-      color: Colors.teal,
-      child: Row(
-        textDirection: TextDirection.ltr,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        verticalDirection: VerticalDirection.down,
-        children: <Widget>[
-          Expanded(
-              child: Text(
-                  'Чрез несколько дней после отъезда Анатоля, Пьер получил записку от князя Андрея, извещавшего '
-                  'его о своем приезде и просившего Пьера заехать к нему.',
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(fontSize: 26)))
-        ],
-      )));
+  runApp(MaterialApp(
+      home: Scaffold(
+    body: Column(children: [
+      Counter(increment: 2),
+      Counter(increment: 1),
+    ]),
+    appBar: AppBar(
+      title: Text("METANIT.COM"),
+      backgroundColor: Colors.amberAccent,
+    ),
+  )));
+}
+
+class Counter extends StatefulWidget {
+  int increment = 1;
+
+  Counter({Key key, this.increment}) : super(key: key); //Не работает, хотя написано так
+  //рабочий вариант
+  //  Counter({Key? key, required this.increment}) : super(key: key);
+
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int value = 0;
+
+  increaseValue() {
+    setState(() {
+      value = value + widget.increment;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        child: Text("Value: $value", style: TextStyle(fontSize: 22)),
+        onPressed: () {
+          increaseValue();
+        });
+  }
 }
